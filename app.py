e8af57f8919958fa2cc0996d4c00ccfed9ed2656
Irfan Syah <106759624+Irfan123fw@users.noreply.github.com>
2024-03-01 13:37:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -24,10 +24,19 @@ Helpful answer:
 """
 
 DATA_PATH = 'content/'
+st.set_page_config(page_title="Chatbot Hadis", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.title("Hadith Question Answering App💬")
+         
+if "messages" not in st.session_state.keys(): # Initialize the chat messages history
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Klik button to processing file"}
+    ]
 
-def initialize_system():
-    if 'responses' not in st.session_state:
-         st.session_state['responses'] = ["Processing"]
+with st.sidebar:
+    st.title('🦙💬 Hadit Chatbot')
+
+if st.button("Process"):
+    st.text("Processing ...")    
     loader = DirectoryLoader(DATA_PATH,
                              glob='*.pdf',
                              loader_cls=PyPDFLoader)
@@ -42,56 +51,57 @@ def initialize_system():
 
 
     message_history = ChatMessageHistory()
-    memory = ConversationBufferMemory(
-        memory_key="chat_history",
-        output_key="answer",
-        chat_memory=message_history,
-        return_messages=True,
-    )
+    if 'memory' not in st.session_state:
 
-    prompt = PromptTemplate(template=custom_prompt_template, input_variables=["context", "question"])
-
-    response = f"Processing done. Silahkan Ajukan Pertanyaan Anda !"
-    st.session_state['responses'] = [(response)]
-    return docsearch, memory, prompt
-
-st.title("Question Answering Pdf Using Gemini-pro")
-
-if "initialized" not in st.session_state:
-    st.session_state.docsearch, st.session_state.memory, st.session_state.prompt = initialize_system()
-    st.session_state.initialized = True
-if 'requests' not in st.session_state:
-    st.session_state['requests'] = []
-# container for chat history
-response_container = st.container()
-# container for text box
-textcontainer = st.container()
-# Initialize variables and objects
-# Streamlit main loop
-with textcontainer:
-    query = st.text_input("Ask your question:")
-    if st.button("Ask"):
-        docsearch = st.session_state.docsearch
-        memory = st.session_state.memory
-        prompt = st.session_state.prompt
-
-        llm = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0, max_output_tokens=2048)
-        chain = ConversationalRetrievalChain.from_llm(
-            llm=llm,
-            chain_type="stuff",
-            retriever=docsearch.as_retriever(),
-            verbose=True,
-            memory=memory,
-            combine_docs_chain_kwargs={"prompt": prompt}
+         st.session_state.memory = ConversationBufferMemory(
+            memory_key="chat_history",
+            output_key="answer",
+            chat_memory=message_history,
+            return_messages=True,
         )
-        results =  chain.run(query)
-        print(results)
-        st.session_state.requests.append(query)
-        st.session_state.responses.append(results) 
-with response_container:
-    if st.session_state['responses']:
 
-        for i in range(len(st.session_state['responses'])):
-            message(st.session_state['responses'][i],key=str(i))
-            if i < len(st.session_state['requests']):
-                message(st.session_state["requests"][i], is_user=True,key=str(i)+ '_user')
+    if 'prompt' not in st.session_state:
+
+         st.session_state.prompt = PromptTemplate(template=custom_prompt_template, input_variables=["context", "question"])
+
+    response = f"Processing done.!"
+    st.session_state.messages = [{"role": "assistant", "content": response}]
+    
+if user_question := st.chat_input("Your question"): # Prompt for user input and save to chat history
+    st.session_state.messages.append({"role": "user", "content": user_question})
+def get_text():
+    global input_text
+    input_text = st.text_input("Ask your Question", key="input")
+    return input_text
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+def clear_chat_history():
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Pilihlah Kitab Apa yang Anda ingin tanyakan"}
+    ]
+st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
+
+# If last message is not from assistant, generate a new response
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            llm = ChatOpenAI(model_name="gpt-3.5-turbo-1106", temperature=0, streaming=True)
+            chain = ConversationalRetrievalChain.from_llm(
+                        llm=llm,
+                        chain_type="stuff",
+                        retriever= st.session_state.compression_retriever,
+                        verbose=True,
+                        max_tokens_limit = 3000,
+                        combine_docs_chain_kwargs={"prompt": st.session_state.prompt},
+                        memory= st.session_state.memory,
+                        return_source_documents=True,
+                    )
+            # st_callback = StreamlitCallbackHandler(st.container())
+            res = chain(user_question)#, callbacks=[st_callback])
+            answer = res["answer"]
+            
+            placeholder = st.empty()
+            placeholder.markdown(answer)
+            message = {"role": "assistant", "content": answer}
+            st.session_state.messages.append(message) # Add response to message history 
